e04b322b6c898623b41a6684e65fef3e7115ab00
Riccardo Omiccioli <riccardo@omiccioli.it>
2023-04-28 18:53:30 +0200

    fix: Improving docker config

--- frontend/index.js
+++ frontend/index.js
@@ -7,6 +7,7 @@ const server = http.createServer((req, res) => {
 	  res.statusCode = 200;
 	  res.setHeader('Content-Type', 'text/plain');
 	  res.end('Hello World this is Riccardo\n');
+		console.log("visited frontend");
 });
 
 server.listen(port, hostname, () => {
