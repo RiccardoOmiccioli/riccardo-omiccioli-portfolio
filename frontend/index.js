e8fe255aff9955a89924c252ed2d17204801f768
Riccardo Omiccioli <48404070+RiccardoOmiccioli@users.noreply.github.com>
2023-05-04 15:24:40 +0200

    Update index.js

--- frontend/index.js
+++ frontend/index.js
@@ -5,7 +5,7 @@ const PORT = 3000;
 
 const app = express();
 app.get('/', (req, res) => {
-	res.send('Hello world, this is riccardo.omiccioli.it 3');
+	res.send('Hello world, this is riccardo.omiccioli.it fwhoiwfhofjoi3');
 });
 
 app.listen(PORT, HOST, () => {
