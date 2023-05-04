501523fd92077d58e8eb2664703d2d3195c18ce9
Riccardo Omiccioli <riccardo@omiccioli.it>
2023-05-04 14:30:23 +0200

    feat: Adding watchtower for automatic deployment on container update

--- frontend/index.js
+++ frontend/index.js
@@ -5,7 +5,7 @@ const PORT = 3000;
 
 const app = express();
 app.get('/', (req, res) => {
-	res.send('Hello world, this is riccardo.omiccioli.it');
+	res.send('Hello world, this is riccardo.omiccioli.it 2');
 });
 
 app.listen(PORT, HOST, () => {
